0ec8a6869069c8a25358194b0fdf3d4d685ec794
Zhan <drudkhoutside@gmail.com>
2026-03-09 22:56:31 +0100

    refactored MessagePOJOConsumer

--- src/main/java/com/zhbohdanchykov/MessagePOJOConsumer.java
+++ src/main/java/com/zhbohdanchykov/MessagePOJOConsumer.java
@@ -1,87 +1,81 @@
 package com.zhbohdanchykov;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import jakarta.jms.*;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVPrinter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Callable;
+import java.util.concurrent.atomic.AtomicInteger;
 
 
-public class MessagePOJOConsumer implements Runnable {
+public class MessagePOJOConsumer implements Callable<Integer> {
 
-    private final Logger logger = LoggerFactory.getLogger(MessagePOJOConsumer.class);
-    private final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
-    private final ObjectMapper mapper = new ObjectMapper().registerModule(new JavaTimeModule());
+    private static final Logger LOGGER = LoggerFactory.getLogger(MessagePOJOConsumer.class);
+    private static final Validator VALIDATOR = Validation.buildDefaultValidatorFactory().getValidator();
+    private static final ObjectMapper MAPPER = new ObjectMapper().registerModule(new JavaTimeModule());
+
     private final Connection connection;
     private final String queue;
-    private static final Object csvLock = new Object();
+    private final BlockingQueue<MessagePOJO> validQueue;
+    private final BlockingQueue<MessagePOJO> invalidQueue;
 
-    public MessagePOJOConsumer(Connection connection, String queue) {
+    public MessagePOJOConsumer(Connection connection, String queue,
+                               BlockingQueue<MessagePOJO> validQueue, BlockingQueue<MessagePOJO> invalidQueue) {
         this.connection = connection;
         this.queue = queue;
+        this.validQueue = validQueue;
+        this.invalidQueue = invalidQueue;
     }
 
     @Override
-    public void run() {
+    public Integer call() {
+        AtomicInteger messageCount = new AtomicInteger(0);
         try (
                 Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
                 MessageConsumer consumer = session.createConsumer(session.createQueue(queue))
         ) {
             boolean reading = true;
+
             while (reading) {
                 Message message = consumer.receive();
                 TextMessage textMessage = (TextMessage) message;
                 String text = textMessage.getText();
+                LOGGER.debug("Message received: {}", text);
                 if (!text.equals("STOP")) {
-                    MessagePOJO messagePOJO = mapper.readValue(text, MessagePOJO.class);
-                    Set<ConstraintViolation<MessagePOJO>> violations = validator.validate(messagePOJO);
-
-                    synchronized (csvLock) {
-                        writeMessageToCSV(messagePOJO, violations);
+                    MessagePOJO messagePOJO = MAPPER.readValue(text, MessagePOJO.class);
+                    Set<ConstraintViolation<MessagePOJO>> violations = VALIDATOR.validate(messagePOJO);
+                    if (violations.isEmpty()) {
+                        validQueue.put(messagePOJO);
+                    } else {
+                        messagePOJO.setErrors(violations.stream().map(ConstraintViolation::getMessage).toList());
+                        invalidQueue.put(messagePOJO);
                     }
+                    messageCount.incrementAndGet();
                 } else {
+                    LOGGER.info("Received poison pill");
                     reading = false;
+                    LOGGER.info("Total messages received: {}", messageCount);
+                    validQueue.put(MessagePOJO.poison());
+                    invalidQueue.put(MessagePOJO.poison());
                 }
             }
         } catch (JMSException e) {
-            logger.error("Failed to start consumer.", e);
-        } catch (IOException e) {
-            logger.error("Failed to write message to CSV.", e);
+            LOGGER.error("Failed to start consumer.", e);
+        } catch (JsonProcessingException e) {
+            LOGGER.error("Failed to parse JSON message.", e);
+        } catch (InterruptedException e) {
+            LOGGER.error("Failed to put a message in a BlockingQueue.", e);
+            Thread.currentThread().interrupt();
         }
-    }
 
-    private void writeMessageToCSV(MessagePOJO messagePOJO, Set<ConstraintViolation<MessagePOJO>> violations)
-            throws IOException {
-        File file = violations.isEmpty() ? new File("valid_messages.csv") :
-                new File("invalid_messages.csv");
-        String[] header = violations.isEmpty() ?
-                new String[]{"name", "count"} :
-                new String[]{"name", "count", "errors"};
-        CSVFormat format = file.exists() ? CSVFormat.DEFAULT : CSVFormat.DEFAULT.withHeader(header);
-        FileWriter out = new FileWriter(file, true);
-
-        List<String> errorsList = violations.stream().map(ConstraintViolation::getMessage).toList();
-        Map<String, List<String>> errors = new HashMap<>();
-        errors.put("errors", errorsList);
-        try (CSVPrinter printer = new CSVPrinter(out, format)) {
-            if (!violations.isEmpty()) {
-                printer.printRecord(messagePOJO.getName(), messagePOJO.getCount(), mapper.writeValueAsString(errors));
-            } else {
-                printer.printRecord(messagePOJO.getName(), messagePOJO.getCount());
-            }
-        }
+        return messageCount.get();
     }
 }
